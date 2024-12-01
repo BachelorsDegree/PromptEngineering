87c913b18f1ed56d85322ff4d51ce0ab4ad2ac50
BachelorsDegree <157184030+BachelorsDegree@users.noreply.github.com>
2024-12-01 23:06:41 +0300

    l8

--- lession8/import_my_module_2.py
+++ lession8/import_my_module_2.py
@@ -2,7 +2,7 @@ import courses
 
 courses.python.hello("Вася")
 print(courses.test)  # This should show you the module, not a string
-courses.test1.divide(8, 4)
+courses.test.divide(8, 4)
 print(dir(courses))
 
 
