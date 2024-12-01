ac181b978398f5dd0ceca8ce936fb8c9e3cfe6ed
BachelorsDegree <157184030+BachelorsDegree@users.noreply.github.com>
2024-12-01 22:38:36 +0300

    l8

--- lession8/import_my_module_2.py
+++ lession8/import_my_module_2.py
@@ -1,5 +1,15 @@
 import courses
+import importlib.util
+import sys
+
+spec = importlib.util.spec_from_file_location("test", "D:\PythonProjects\PromptEngineering\lession8\courses\test.py")
+test = importlib.util.module_from_spec(spec)
+sys.modules["module.name"] = test
+spec.loader.exec_module(test)
 
 courses.python.hello("Вася")
+print(courses.test)  # This should show you the module, not a string
 courses.test.divide(8, 4)
 print(dir(courses))
+
+
