6483ad1cf5cbddb01543812ae2a8d5d3459cfdc7
BachelorsDegree <157184030+BachelorsDegree@users.noreply.github.com>
2024-12-08 23:54:33 +0300

    l8

--- lession8/working_with_files1.py
+++ lession8/working_with_files1.py
@@ -12,47 +12,41 @@ def detect_file_encoding(file_path):
         result = chardet.detect(raw_data)
         return result['encoding']
 
-file_path = 'new_text_file.txt'  # Replace with your file path
+file_path = 'new_text_file.txt'  # Замените на путь к вашему файлу
 encoding = detect_file_encoding(file_path)
 print(f"The encoding of the file is: {encoding}") # Вывод кодировки
 
 # Функция для вывода текста с указанной кодировкой
 def print_with_encoding(text, encoding='utf-8'):
-    # Сохраняем оригинальный sys.stdout
-    original_stdout = sys.stdout
-
     # Перенаправляем sys.stdout на TextIOWrapper с нужной кодировкой
-    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding=encoding)
+    wrapped_stdout = io.TextIOWrapper(sys.stdout.buffer, encoding=encoding)
     
     try:
         # Выводим текст
-        sys.stdout.write(text + '\n')
+        wrapped_stdout.write(text + '\n')
     finally:
-        # Возвращаем оригинальный sys.stdout
-        sys.stdout = original_stdout
+        # Закрываем wrapped_stdout, но не sys.stdout
+        wrapped_stdout.detach()
 
 # Пример использования
 # print_with_encoding("Hello, world! Привет, мир!", encoding='utf-8')
 # print_with_encoding("Hello, world! Привет, мир!", encoding='iso-8859-1')
 
-file = open("new_text_file.txt") # Открываем файл
-text = file.read( ) # Считываем содержимое файла
-print(text + "\n") # Выводим содержимое на экран
+file = open("new_text_file.txt", encoding=encoding) # Открываем файл с определенной кодировкой
+text = file.read() # Считываем содержимое файла
+print(text) # Выводим содержимое на экран
 
 # Рассмотрим ряд функций для взаимодействия с файлом:
 file.seek(0) # Перемещаемся в начало файла
 text = file.read(34) # Считываем определенное количество символов
-print_with_encoding(text, encoding='cp1251')
+print_with_encoding(text, encoding='utf-8')
 
 file.seek(0)
-text = file.readline () # Считываем одну строку
-print_with_encoding(text, encoding='cp1251')
+text = file.readline() # Считываем одну строку
+print_with_encoding(text, encoding='utf-8')
 
 file.seek(0)
-text = file.readlines ( ) # Считываем все строки
-print_with_encoding(text, encoding='cp1251')
+text = file.readlines() # Считываем все строки
+print_with_encoding(''.join(text), encoding='utf-8')
 
-
-file.close ( ) # Закрываем файл
-
-
+file.close() # Закрываем файл
