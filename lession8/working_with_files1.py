a65d7dd45a42bbc68d33f8d3e7f4aa77ad660e7d
BachelorsDegree <157184030+BachelorsDegree@users.noreply.github.com>
2024-12-09 00:43:45 +0300

    l8

--- lession8/working_with_files1.py
+++ lession8/working_with_files1.py
@@ -1,9 +1,24 @@
 # Подключаем модуль chardet
 import io
+import locale
 import sys
 
 import chardet
 
+
+def print_system_encoding():
+    # Получаем кодировку системы
+    system_encoding = locale.getpreferredencoding()
+    print(f"Системная кодировка: {system_encoding}")
+
+print_system_encoding()
+
+def print_console_encoding():
+    # Получаем кодировку стандартного вывода
+    encoding = sys.stdout.encoding
+    print(f"Кодировка консоли: {encoding}")
+
+print_console_encoding()
 
 # Функция для определения кодировки
 def detect_file_encoding(file_path):
@@ -14,7 +29,7 @@ def detect_file_encoding(file_path):
 
 file_path = 'new_text_file.txt'  # Замените на путь к вашему файлу
 encoding = detect_file_encoding(file_path)
-print(f"The encoding of the file is: {encoding}") # Вывод кодировки
+print(f"Кодировка файла: {encoding}") # Вывод кодировки
 
 # Функция для вывода текста с указанной кодировкой
 def print_with_encoding(text, encoding='utf-8'):
@@ -32,21 +47,27 @@ def print_with_encoding(text, encoding='utf-8'):
 # print_with_encoding("Hello, world! Привет, мир!", encoding='utf-8')
 # print_with_encoding("Hello, world! Привет, мир!", encoding='iso-8859-1')
 
-file = open("new_text_file.txt", encoding=encoding) # Открываем файл с определенной кодировкой
+file = open("new_text_file.txt") # Открываем файл с определенной кодировкой
+# ИЛИ file = open("new_text_file.txt", encoding=encoding) и тогда, 
+# дальше можно кодировку не указывать или указать utf-8(.txt файл в utf-8)
 text = file.read() # Считываем содержимое файла
+print("Печатаем текст из файла с кодировкой utf-8 в открытого в кодировке cp1251:")
+print("\n")
 print(text) # Выводим содержимое на экран
 print("\n")
 # Рассмотрим ряд функций для взаимодействия с файлом:
+
+print("Используем функцию для вывода текста из файла с кодировкой utf-8 в текст в кодировке cp1251:")
 file.seek(0) # Перемещаемся в начало файла
 text = file.read(34) # Считываем определенное количество символов
-print_with_encoding(text)
+print_with_encoding(text, encoding='cp1251')
 print("\n")
 file.seek(0)
 text = file.readline() # Считываем одну строку
-print_with_encoding(text)
+print_with_encoding(text, encoding='cp1251')
 print("\n")
 file.seek(0)
 text = file.readlines() # Считываем все строки
-print_with_encoding(''.join(text))
+print_with_encoding(''.join(text), encoding='cp1251')
 print("\n")
 file.close() # Закрываем файл
